5b673d327016738794a2e6ecb67c38fd3ba14b7d
JeffreyFG <JeffreyFulmerGardner@outlook.com>
2020-04-19 14:43:38 -0700

    Corrected the route.

--- application/routes/registered.js
+++ application/routes/registered.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/Registered', function(req, res, next) {
-  res.sendFile('registration.html');
+  res.sendFile('registered.html');
 });
 
 module.exports = router;
