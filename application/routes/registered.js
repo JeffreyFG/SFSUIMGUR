417dd972aef5fa27c1fb6ceaf52ec2681ed3fd40
JeffreyFG <JeffreyFulmerGardner@outlook.com>
2020-04-19 15:51:21 -0700

    I added the second router inside the registeredrouter

--- application/routes/registered.js
+++ application/routes/registered.js
@@ -3,9 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/registered', function(req, res, next) {
-  res.redirect('registered.html');
-  //res.sendFile('registered.html');
+  res.redirect('/registeredcomplete');
+  //
 
+});
+router.get('/registeredcomplete',function(req,res,next){
+    res.sendFile('registered.html');
 });
 
 module.exports = router;
