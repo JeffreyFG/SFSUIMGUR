183442f8a37a3575b013ff8f39781330be3f5ce6
JeffreyFG <JeffreyFulmerGardner@outlook.com>
2020-04-19 14:47:27 -0700

    trying to redirect to work

--- application/routes/registered.js
+++ application/routes/registered.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/Registered', function(req, res, next) {
+  res.redirect('registered.html');
   res.sendFile('registered.html');
+
 });
 
 module.exports = router;
